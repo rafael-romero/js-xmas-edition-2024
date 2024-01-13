c49be5380b1507ae1e6db90f0ac64318e71e6b01
Rafael Romero <rafa710@gmail.com>
2024-01-13 06:24:40 -0300

    realizacion de la tarea de la clase 7

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
- 
- 
+const $form = document.querySelector("#carta-a-santa");
+
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcionRegalo = $form["descripcion-regalo"].value;
+
+function validarNombre(nombre) {
+  const cantidadMaximaDeCaracteres = 50;
+
+  if (nombre.length === 0) {
+    return "Este campo debe tener al menos 1 caracter";
+  } else if (nombre.length >= cantidadMaximaDeCaracteres) {
+    return "Este campo debe tener menos de 50 caracteres";
+  } else {
+    return "";
+  }
+}
+console.log(validarNombre(nombre));
+
+function validarCiudad(ciudad) {
+  if (ciudad.length === 0) {
+    return "Debe seleccionar una ciudad";
+  } else {
+    return "";
+  }
+}
+console.log(validarCiudad(ciudad));
+
+function validarDescripcionRegalo(descripcionRegalo) {
+  const cantidadMaximaDeCaracteres = 200;
+  if (descripcionRegalo.length === 0) {
+    return "Debe describir que le gustaria recibir este año";
+  } else if (descripcionRegalo.length >= cantidadMaximaDeCaracteres) {
+    return "Este campo debe tener menos de 200 caracteres";
+  } else {
+    return "";
+  }
+}
+
+console.log(validarDescripcionRegalo(descripcionRegalo));
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,39 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre("") === "Este campo debe tener al menos 1 caracter",
+    "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre(
+      "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "Este campo debe tener menos de 50 caracteres",
+    "Validar nombre no validó que el nombre sea menor a 50 caracteres"
   );
 }
 
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad("") === "Debe seleccionar una ciudad",
+    "Validar ciudad no valido que haya selecionado una opcion valida"
+  );
+}
+
+function probarValidarDescripcionRegalo() {
+  console.assert(
+    validarDescripcionRegalo("") ===
+      "Debe describir que le gustaria recibir este año",
+    "Validar descripcion regalo, no valido que el campo no este vacio"
+  );
+  console.assert(
+    validarDescripcionRegalo(
+      "1111111111111111111111111111212121221212121212121212121212121212121212121212121212121212121212121212121212121221212121212121213541653465465416513213213132111111111111111111111111111121212122121212121212121212121212121212121212121212121212121212121212121212121212122121212121212121354165346546541651321321313211111111111111111111111111112121212212121212121212121212121212121212121212121212121212121212121212121212121212212121212121212135416534654654165132132131321111111111111111111111111111212121221212121212121212121212121212121212121212121212121212121212121212121212121221212121212121213541653465465416513213213132"
+    ) === "Este campo debe tener menos de 200 caracteres",
+    "Validar descripcion regalo, no pudo validar que el campo tenga menos de 200 caracteres"
+  );
+}
+
+
 probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
